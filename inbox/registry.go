debbf7524dca551b0953386ff141205c2b439c65
Kirill Sedin <k.sedin@qlean.ru>
2023-05-19 21:27:37 +0500

    doc(inbox): add doc

--- inbox/registry.go
+++ inbox/registry.go
@@ -15,6 +15,7 @@ type Handler interface {
 	Process(context.Context, []byte) error
 }
 
+// Registry contains all handler that will be processed by Inbox.
 type Registry struct {
 	eventMap *eventMap
 }
@@ -25,6 +26,34 @@ func NewRegistry() *Registry {
 	}
 }
 
+// On register new handlers to specific event key. All handlers
+// will be executed on received event with provided key.
+//
+// Example:
+//
+//	  We have
+//	   - event type = "order_events"
+//	   - handler key = "process_order"
+//	   - handler key = "update_order"
+//	  The registry will bind the keys "process_order", "update_order"
+//	  with event "order_events" and execute both registered handlers
+//	  for each received event with event type "order_events".
+//
+//	  We have
+//	  - event type = "order_events"
+//	  - handler key = "process_order"
+//	  - handler key = "process_order"
+//		 If we are trying to provide several multiple handlers with the same key,
+//	  then only the first handler will be associated with the event type.
+//	  The second handler will be ignored.
+//
+//	  We have
+//	  - event type = "order_events"
+//	  - registered handler with key = "process_order"
+//	  - new handler key = "process_order"
+//	  If you are trying to provide a handler to an already existing event type, for example,
+//	  "order_events", and the handler has the same key as already provided, then
+//	  this handler will be ignored.
 func (r *Registry) On(event string, handlers ...Handler) {
 	var (
 		correctHandlers = make([]Handler, 0)
@@ -51,6 +80,8 @@ func (r *Registry) On(event string, handlers ...Handler) {
 	r.eventMap.Set(event, correctHandlers...)
 }
 
+// Handlers returns map where key is event type and values are handlers
+// associated to this event type.
 func (r *Registry) Handlers() map[string][]Handler {
 	return r.eventMap.Copy()
 }
